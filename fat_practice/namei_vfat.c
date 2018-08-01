86f77df268096efab05564923b8bb36fe0a6ba8d
ChoiGunHee <godrjsmgl@gmail.com>
2018-08-01 21:31:50 +0900

    mount name display

--- fat_practice/namei_vfat.c
+++ fat_practice/namei_vfat.c
@@ -1074,6 +1074,7 @@ static struct dentry *vfat_mount(struct file_system_type *fs_type,
 		       int flags, const char *dev_name,
 		       void *data)
 {
+	printk(KERN_NOTICE"this mount choigunhee\n");
 	return mount_bdev(fs_type, flags, dev_name, data, vfat_fill_super);
 }
 
